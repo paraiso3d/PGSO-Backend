254c250a17695c9d500b2393fde893ab65b29809
paraiso3d <157773073+paraiso3d@users.noreply.github.com>
2025-04-28 01:03:12 +0800

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -7,23 +7,24 @@ return [
     | Cross-Origin Resource Sharing (CORS) Configuration
     |--------------------------------------------------------------------------
     |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    // You want to allow CORS not only for APIs, but also for public images
+    'paths' => [
+        'api/*',
+        'sanctum/csrf-cookie',
+        'public/img/*', // <- add this if your route is public/img/*
+    ],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['*'], // allow all HTTP methods (GET, POST, etc.)
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'http://localhost:3000', // <- allow only your frontend
+    ],
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => ['*'], // allow all headers
 
     'exposed_headers' => [],
 
